c60868501dbf62278145b60fa0f2db9875b87a7e
Timo Poppinga <timo.poppinga@zdrei.com>
2024-01-01 18:18:01 +0100

    change url

--- z3cli.rb
+++ z3cli.rb
@@ -1,10 +1,10 @@
 class Z3cli < Formula
   desc "The zdrei.com developer console"
   homepage "https://zdrei.com/"
-  url "https://console.z3.ag/z3-console.phar"
+  url "https://github.com/zdrei-com/homebrew-cli/raw/main/z3cli.phar"
   sha256 "5aa883ff46d145a1677e617c64719f66d88b9096f9c0fa0c97ab3298dee3de3f"
   license "property"
-  version "0.0.3"
+  version "0.0.4"
 
   bottle do
   end
@@ -12,7 +12,7 @@ class Z3cli < Formula
   depends_on "php"
 
   def install
-    bin.install "z3-console.phar" => "z3"
+    bin.install "z3cli.phar" => "z3"
   end
 
   test do
