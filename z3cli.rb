858668d626d9d3be73d2243fb4858ff69bc1b21c
Timo Poppinga <timo.poppinga@zdrei.com>
2024-01-02 10:52:41 +0100

    add version

--- z3cli.rb
+++ z3cli.rb
@@ -2,7 +2,7 @@ class Z3cli < Formula
   desc "The zdrei.com developer console"
   homepage "https://zdrei.com/"
   url "https://github.com/zdrei-com/homebrew-cli/raw/main/z3cli.phar"
-  sha256 "6410e83344e6835563db79312e8dd2ed77b3d2902a2fccf281ed2ca23bb5c8ba"
+  sha256 "5a2571473ad7d5413f00255f8a7d253da517180fe5ca8558058064cfe6014e1d"
   license "property"
   version "0.0.6"
 
