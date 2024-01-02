2cd00af910e5c0d122077819e15956b42d431158
Timo Poppinga <timo.poppinga@zdrei.com>
2024-01-02 11:20:53 +0100

    [TASK] #2 Add support to check for git /s19

--- z3cli.rb
+++ z3cli.rb
@@ -1,10 +1,10 @@
 class Z3cli < Formula
   desc "The zdrei.com developer console"
   homepage "https://zdrei.com/"
-  url "https://github.com/zdrei-com/homebrew-cli/raw/main/z3cli.phar"
-  sha256 "5aa883ff46d145a1677e617c64719f66d88b9096f9c0fa0c97ab3298dee3de3f"
+  url "https://github.com/zdrei-com/homebrew-cli/raw/main/z3cli_0.0.7.phar"
+  sha256 "ca2e9c0b32a98648b2bfbbde16ae674d98c44fc553391b1250ed8a621c4b0357"
   license "property"
-  version "0.0.6"
+  version "0.0.7"
 
   bottle do
   end
