632e15f64b5505d0f79eb083fe64ddf9a7927eaa
Timo Poppinga <timo.poppinga@zdrei.com>
2024-01-02 12:43:42 +0100

    [TASK] #2 relase version 0.0.9 /s28

--- z3cli.rb
+++ z3cli.rb
@@ -1,10 +1,10 @@
 class Z3cli < Formula
   desc "The zdrei.com developer console"
   homepage "https://zdrei.com/"
-  url "https://github.com/zdrei-com/homebrew-cli/raw/main/z3cli_0.0.8.phar"
-  sha256 "5954018fd3148e8c50a2e86f9ecc33f324c28677a91f0ad97c5ee5adb58e74a9"
+  url "https://github.com/zdrei-com/homebrew-cli/raw/main/z3cli_0.0.9.phar"
+  sha256 "0508fd1d1fd885271e5e660543ec8f327d53e74e8974918f261005d88fbf362a"
   license "property"
-  version "0.0.8"
+  version "0.0.9"
 
   bottle do
   end
@@ -12,7 +12,7 @@ class Z3cli < Formula
   depends_on "php"
 
   def install
-    bin.install "z3cli_0.0.8.phar" => "z3"
+    bin.install "z3cli_0.0.9.phar" => "z3"
   end
 
   test do
