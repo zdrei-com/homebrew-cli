330795986ab2893afc27fe04c2f4570d9599688d
Timo Poppinga <timo.poppinga@zdrei.com>
2024-01-02 13:18:47 +0100

    [TASK] #2 add support for sppend relase 0.0.10 and fix path issue /s34

--- z3cli.rb
+++ z3cli.rb
@@ -1,10 +1,10 @@
 class Z3cli < Formula
   desc "The zdrei.com developer console"
   homepage "https://zdrei.com/"
-  url "https://github.com/zdrei-com/homebrew-cli/raw/main/z3cli_0.0.9.phar"
-  sha256 "0508fd1d1fd885271e5e660543ec8f327d53e74e8974918f261005d88fbf362a"
+  url "https://github.com/zdrei-com/homebrew-cli/raw/main/z3cli_0.0.10.phar"
+  sha256 "e7a0b1a7740031ae8bc8ff60d3df88b715768e2db2602ce1941e0a8d634e312a"
   license "property"
-  version "0.0.9"
+  version "0.0.10"
 
   bottle do
   end
@@ -12,7 +12,7 @@ class Z3cli < Formula
   depends_on "php"
 
   def install
-    bin.install "z3cli_0.0.9.phar" => "z3"
+    bin.install "z3cli_0.0.10.phar" => "z3"
   end
 
   test do
